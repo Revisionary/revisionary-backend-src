bb90724e36458c967136a67e66227b3d72a19c5d
Bilal TAS <bilaltas@me.com>
2017-06-26 14:55:46 +0300

    DB Backup

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 25, 2017 at 03:30 AM
+-- Generation Time: Jun 26, 2017 at 02:54 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.5
 
@@ -47,6 +47,64 @@ INSERT INTO `categories` (`cat_ID`, `cat_name`, `cat_type`, `cat_user_ID`) VALUE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `devices`
+--
+
+CREATE TABLE `devices` (
+  `device_ID` bigint(20) NOT NULL,
+  `device_name` text NOT NULL,
+  `device_width` mediumint(10) NOT NULL,
+  `device_height` mediumint(10) NOT NULL,
+  `device_rotateable` tinyint(1) NOT NULL DEFAULT '0',
+  `device_color` varchar(10) DEFAULT NULL,
+  `device_frame` varchar(15) DEFAULT NULL,
+  `device_cat_ID` bigint(20) NOT NULL,
+  `device_order` bigint(20) NOT NULL,
+  `device_user_ID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `devices`
+--
+
+INSERT INTO `devices` (`device_ID`, `device_name`, `device_width`, `device_height`, `device_rotateable`, `device_color`, `device_frame`, `device_cat_ID`, `device_order`, `device_user_ID`) VALUES
+(1, 'iMac 27', 2560, 1440, 0, NULL, NULL, 1, 0, 1),
+(2, 'iMac 21', 1920, 1080, 0, NULL, NULL, 1, 1, 1),
+(3, 'Macbook Pro 17', 1920, 1200, 0, NULL, NULL, 1, 2, 1),
+(4, 'Macbook Pro 15', 1440, 900, 0, NULL, NULL, 1, 3, 1),
+(5, 'Macbook Pro 13', 1280, 800, 0, NULL, NULL, 1, 4, 1),
+(6, 'iPad', 768, 1024, 1, NULL, NULL, 2, 0, 1),
+(7, 'iPhone 6 Plus, 6S Plus, 7 Plus', 414, 736, 1, NULL, NULL, 3, 0, 1),
+(8, 'iPhone 6, 6S, 7', 375, 667, 1, NULL, NULL, 3, 1, 1),
+(9, 'iPhone 5, 5C, 5S, SE', 320, 568, 1, NULL, NULL, 3, 2, 1),
+(10, 'iPhone 4 & 4S', 320, 480, 1, NULL, NULL, 3, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device_categories`
+--
+
+CREATE TABLE `device_categories` (
+  `device_cat_ID` bigint(20) NOT NULL,
+  `device_cat_name` varchar(20) NOT NULL,
+  `device_cat_icon` varchar(20) NOT NULL,
+  `device_cat_order` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `device_categories`
+--
+
+INSERT INTO `device_categories` (`device_cat_ID`, `device_cat_name`, `device_cat_icon`, `device_cat_order`) VALUES
+(1, 'Desktop', 'fa-desktop', 0),
+(2, 'Tablet', 'fa-tablet', 1),
+(3, 'Mobile', 'fa-mobile', 2),
+(4, 'Custom', 'fa-window-restore', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pages`
 --
 
@@ -60,6 +118,8 @@ CREATE TABLE `pages` (
   `page_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `page_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `project_ID` bigint(20) NOT NULL,
+  `device_ID` bigint(20) NOT NULL,
+  `parent_page_ID` bigint(20) DEFAULT NULL,
   `user_ID` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -67,18 +127,21 @@ CREATE TABLE `pages` (
 -- Dumping data for table `pages`
 --
 
-INSERT INTO `pages` (`page_ID`, `page_name`, `page_pic`, `page_url`, `page_archived`, `page_deleted`, `page_created`, `page_modified`, `project_ID`, `user_ID`) VALUES
-(1, 'About', 'about.png', 'https://www.twelve12.com/about-us/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(2, 'Contact', 'contact.png', 'https://www.twelve12.com/contact/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(3, 'GM Properties', 'gm.png', 'https://www.twelve12.com/project/gm-properties/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(4, '128 Online', '128.png', 'https://www.twelve12.com/project/128-online-store/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(5, 'Vampire Tools', 'vampire.png', 'https://www.twelve12.com/project/vampire-tools/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(6, 'inMotion', 'inmotion.png', 'https://www.twelve12.com/project/inmotion/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(7, 'The Kitchen', 'kitchen.png', 'https://www.twelve12.com/project/the-kitchen-at-westwood/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(8, 'Blog 1', 'blog1.png', 'https://www.twelve12.com/blog/branding/brand-way-box/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(9, 'Blog 2', 'blog2.png', 'https://www.twelve12.com/blog/branding/branding-tips/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 1),
-(10, 'Home', 'home.png', 'https://www.twelve12.com/', 0, 0, '2017-06-24 14:38:34', '2017-06-24 14:38:34', 8, 2),
-(11, 'Bilal\'s Home', 'bilal.png', 'http://www.bilaltas.net/', 0, 0, '2017-06-25 00:21:09', '2017-06-25 00:21:09', 1, 1);
+INSERT INTO `pages` (`page_ID`, `page_name`, `page_pic`, `page_url`, `page_archived`, `page_deleted`, `page_created`, `page_modified`, `project_ID`, `device_ID`, `parent_page_ID`, `user_ID`) VALUES
+(1, 'About', 'about.png', 'https://www.twelve12.com/about-us/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(2, 'Contact', 'contact.png', 'https://www.twelve12.com/contact/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(3, 'GM Properties', 'gm.png', 'https://www.twelve12.com/project/gm-properties/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(4, '128 Online', '128.png', 'https://www.twelve12.com/project/128-online-store/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(5, 'Vampire Tools', 'vampire.png', 'https://www.twelve12.com/project/vampire-tools/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(6, 'inMotion', 'inmotion.png', 'https://www.twelve12.com/project/inmotion/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(7, 'The Kitchen', 'kitchen.png', 'https://www.twelve12.com/project/the-kitchen-at-westwood/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(8, 'Blog 1', 'blog1.png', 'https://www.twelve12.com/blog/branding/brand-way-box/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(9, 'Blog 2', 'blog2.png', 'https://www.twelve12.com/blog/branding/branding-tips/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 4, NULL, 1),
+(10, 'Home', 'home.png', 'https://www.twelve12.com/', 0, 0, '2017-06-24 14:38:34', '2017-06-24 14:38:34', 8, 4, NULL, 2),
+(11, 'Bilal\'s Home', 'bilal.png', 'http://www.bilaltas.net/', 0, 0, '2017-06-25 00:21:09', '2017-06-25 00:21:09', 1, 4, NULL, 1),
+(12, 'Anasayfa', 'cuneyt.png', 'http://www.cuneyt-tas.com/', 0, 0, '2017-06-25 11:20:36', '2017-06-25 11:20:36', 5, 4, NULL, 5),
+(13, 'BBC Home', 'bbc.png', 'http://www.bbc.com/', 0, 0, '2017-06-26 08:05:02', '2017-06-26 08:05:02', 6, 4, NULL, 1),
+(14, 'About', 'about.png', 'https://www.twelve12.com/about-us/', 0, 0, '2017-06-24 14:20:44', '2017-06-24 14:20:44', 8, 7, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -107,7 +170,8 @@ INSERT INTO `page_cat_connect` (`page_cat_connect_ID`, `page_cat_page_ID`, `page
 (7, 7, 4, 1),
 (8, 8, 5, 1),
 (9, 9, 5, 1),
-(10, 10, 3, 1);
+(10, 10, 3, 1),
+(11, 14, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -134,7 +198,7 @@ INSERT INTO `projects` (`project_ID`, `project_name`, `project_pic`, `project_ar
 (2, 'SoundCloud', 'soundcloud.png', 0, 0, '2017-06-18 15:26:34', 1),
 (3, 'TWSJ', 'twsj.png', 0, 1, '2017-06-18 15:27:35', 1),
 (4, 'Youtube', 'youtube.png', 0, 0, '2017-06-18 15:28:12', 1),
-(5, 'Cuneyt TAS', 'cuneyt-tas.png', 0, 0, '2017-06-18 15:28:34', 4),
+(5, 'Cuneyt TAS', 'cuneyt-tas.png', 0, 0, '2017-06-18 15:28:34', 5),
 (6, 'BBC', 'bbc.png', 0, 0, '2017-06-18 15:29:25', 1),
 (7, 'Envato', 'envato.png', 0, 0, '2017-06-18 15:29:25', 1),
 (8, 'Twelve12', 'twelve12.png', 0, 0, '2017-06-18 17:54:24', 2);
@@ -224,7 +288,8 @@ INSERT INTO `users` (`user_ID`, `user_name`, `user_email`, `user_password`, `use
 (1, 'bilaltas', 'bilaltas@me.com', '$2y$10$FlJ0PwBy6.5m8MXqIDMv5u.CsTW9w7bEgmlzLUCG9il6ZaN6KMmVC', 'Bilal', 'TAS', 'bill.png', 1, '2017-03-31 22:18:00', 1),
 (2, 'ike', 'ike@twelve12.com', '$2y$10$UcoctpiTNtf9grzFmz53lut7X4l3EBspRC4xz/Qn/qJ1VWWIq81n.', 'Ike', 'Elimsa', 'ike.png', 0, '2017-06-17 22:28:00', 4),
 (3, 'sara', 'sara@twelve12.com', '$2y$10$a7njFOCmPx.hJVvunpqeBe6HskuWJiubX0rawUDxvpP.s3VX4/CI2', 'Sara', 'Atalay', 'sara.png', 0, '2017-06-17 22:28:00', 3),
-(4, 'cuneyt-tas', 'cuneyttas@hotmail.com.tr', '$2y$10$XIXCUmV38NyLeh13EiqynuuywixfNkWOt6.S5Llezkz1E1WBQQVDa', 'Cüneyt', 'TAŞ', NULL, 0, '2017-06-18 13:51:00', 2);
+(4, 'matt', 'metin@twelve12.com', '$2y$10$a7njFOCmPx.hJVvunpqeBe6HskuWJiubX0rawUDxvpP.s3VX4/CI2', 'Matt', 'Pasaoglu', 'matt.png', 0, '2017-06-18 13:51:00', 2),
+(5, 'joey-goksu', 'joey@twelve12.com', '$2y$10$bAgxLbJhGEkO4HLXknHcPeZuGqRotBlb5G2qpCx4D0ARhRoveidMq', 'Joey', 'Goksu', 'joey.png', 0, '2017-06-25 09:28:07', 2);
 
 -- --------------------------------------------------------
 
@@ -268,12 +333,27 @@ ALTER TABLE `categories`
   ADD KEY `project_cat_user_ID` (`cat_user_ID`);
 
 --
+-- Indexes for table `devices`
+--
+ALTER TABLE `devices`
+  ADD PRIMARY KEY (`device_ID`),
+  ADD KEY `device_cat_ID` (`device_cat_ID`),
+  ADD KEY `user_ID` (`device_user_ID`);
+
+--
+-- Indexes for table `device_categories`
+--
+ALTER TABLE `device_categories`
+  ADD PRIMARY KEY (`device_cat_ID`);
+
+--
 -- Indexes for table `pages`
 --
 ALTER TABLE `pages`
   ADD PRIMARY KEY (`page_ID`),
   ADD KEY `user_ID` (`user_ID`),
-  ADD KEY `project_ID` (`project_ID`);
+  ADD KEY `project_ID` (`project_ID`),
+  ADD KEY `device_ID` (`device_ID`);
 
 --
 -- Indexes for table `page_cat_connect`
@@ -339,15 +419,25 @@ ALTER TABLE `user_levels`
 ALTER TABLE `categories`
   MODIFY `cat_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
+-- AUTO_INCREMENT for table `devices`
+--
+ALTER TABLE `devices`
+  MODIFY `device_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `device_categories`
+--
+ALTER TABLE `device_categories`
+  MODIFY `device_cat_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `page_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `page_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `page_cat_connect`
 --
 ALTER TABLE `page_cat_connect`
-  MODIFY `page_cat_connect_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `page_cat_connect_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `projects`
 --
@@ -372,7 +462,7 @@ ALTER TABLE `sorting`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user_levels`
 --
@@ -389,11 +479,19 @@ ALTER TABLE `categories`
   ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`cat_user_ID`) REFERENCES `users` (`user_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Constraints for table `devices`
+--
+ALTER TABLE `devices`
+  ADD CONSTRAINT `devices_ibfk_1` FOREIGN KEY (`device_cat_ID`) REFERENCES `device_categories` (`device_cat_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `devices_ibfk_2` FOREIGN KEY (`device_user_ID`) REFERENCES `users` (`user_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Constraints for table `pages`
 --
 ALTER TABLE `pages`
   ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`user_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `pages_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `pages_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `projects` (`project_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pages_ibfk_3` FOREIGN KEY (`device_ID`) REFERENCES `devices` (`device_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `page_cat_connect`
