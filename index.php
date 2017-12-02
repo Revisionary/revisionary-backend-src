555c99cfe03255892897c08c4618001652a00d22
Bilal TAS <bilaltas@me.com>
2017-12-02 14:05:59 +0300

    Force HTTPS on all pages except "revise"

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ require 'app/init.php';
 $_url = get('url');
 $_url = array_filter(explode('/', $_url));
 
+
 if(!isset($_url[0])){
   $_url[0] = 'index';
 }
+
 if(!file_exists(controller($_url[0]))){
 	http_response_code(404);
 
@@ -18,6 +20,15 @@ if(!file_exists(controller($_url[0]))){
 	if(file_exists(controller('404')))
 		$_url[0] = '404';
 }
+
+// Force HTTPS
+if (!ssl && $_url[0] != 'revise' && $_url[0] != 'ajax') {
+
+	header( 'Location: '.current_url(null, null, true) );
+	die();
+
+}
+
 
 ob_start();
 require controller($_url[0]);
