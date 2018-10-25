923d0f10bd81e3bbd279a79ab651768ab8bd8ada
Bilal TAS <bilaltas@me.com>
2018-10-25 13:42:03 +0300

    Font Awesome updated

--- www/app/view/static/header_html.php
+++ www/app/view/static/header_html.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8">
 		<title><?=$page_title?></title>
 
-		<link rel="stylesheet" href="<?=asset_url('fonts/font-awesome/css/font-awesome.css').'?v='.filemtime(dir.'/assets/fonts/font-awesome/css/font-awesome.css')?>" media="screen">
-		<link rel="stylesheet" href="<?=asset_url('styles/style.css').'?v='.filemtime(dir.'/assets/styles/style.css')?>" media="screen">
+		<link rel="stylesheet" href="<?=asset_url_nocache('fonts/font-awesome/css/all.css')?>" media="screen">
+		<link rel="stylesheet" href="<?=asset_url_nocache('styles/style.css')?>" media="screen">
 
 		<?php
 
@@ -13,9 +13,7 @@
 			if (isset($additionalCSS) && is_array($additionalCSS) ) {
 
 				foreach ($additionalCSS as $file) {
-					$filePath = 'styles/'.$file;
-
-					echo '<link rel="stylesheet" href="'.asset_url($filePath).'?v='.filemtime(dir."/assets/".$filePath).'" media="screen">';
+					echo '<link rel="stylesheet" href="'.asset_url_nocache('styles/'.$file).'" media="screen">';
 				}
 
 			}
@@ -29,9 +27,7 @@
 			if (isset($additionalHeadJS) && is_array($additionalHeadJS) ) {
 
 				foreach ($additionalHeadJS as $file) {
-					$filePath = 'scripts/'.$file;
-
-					echo '<script src="'.asset_url($filePath).'?v='.filemtime(dir."/assets/".$filePath).'"></script>';
+					echo '<script src="'.asset_url_nocache('scripts/'.$file).'"></script>';
 				}
 
 			}
