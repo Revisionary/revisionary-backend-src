56061e07b606d1263574b220d5046ca7e94392d3
Bilal TAS <bilaltas@me.com>
2018-10-26 15:48:47 +0300

    Some arrangements on modals

--- www/app/view/dynamic/modals.php
+++ www/app/view/dynamic/modals.php
@@ -3,78 +3,120 @@ function print_device_selector() {
 	global $db;
 ?>
 
-					<h3 style="margin-bottom: 0">Devices <i class="fa fa-question-circle tooltip" data-tooltip="Custom device option is not working right now." aria-hidden="true"></i></h3>
-					<ul class="selected-devices">
-						<li><?php $custom_device_ID = 11; ?>
-							<input type="hidden" name="devices[]" value="11"/>
-							<input type="hidden" name="page-width" value="<?=Device::ID($custom_device_ID)->getDeviceInfo('device_width')?>"/>
-							<input type="hidden" name="page-height" value="<?=Device::ID($custom_device_ID)->getDeviceInfo('device_height')?>"/>
-							<i class="fa fa-window-maximize" aria-hidden="true"></i> <span>Current Screen (<span class="screen-width"><?=Device::ID($custom_device_ID)->getDeviceInfo('device_width')?></span> x <span class="screen-height"><?=Device::ID($custom_device_ID)->getDeviceInfo('device_height')?></span>)</span>
-							<a href="#" class="remove-device" style="display: none;"><i class="fa fa-times-circle" aria-hidden="true"></i></a>
-						</li>
-					</ul>
-					<span class="dropdown-container">
+<h3 style="margin-bottom: 0">Screen Size <i class="fa fa-question-circle tooltip" data-tooltip="Add your device size that you wish to edit your site." aria-hidden="true"></i></h3>
+<ul class="selected-devices">
+	<li><?php $custom_device_ID = 11; ?>
+		<input type="hidden" name="devices[]" value="11"/>
+		<input type="hidden" name="page-width" value="<?=Device::ID($custom_device_ID)->getDeviceInfo('device_width')?>"/>
+		<input type="hidden" name="page-height" value="<?=Device::ID($custom_device_ID)->getDeviceInfo('device_height')?>"/>
+		<i class="fa fa-window-maximize" aria-hidden="true"></i> <span>Current Screen (<span class="screen-width"><?=Device::ID($custom_device_ID)->getDeviceInfo('device_width')?></span> x <span class="screen-height"><?=Device::ID($custom_device_ID)->getDeviceInfo('device_height')?></span>)</span>
+		<a href="#" class="remove-device" style="display: none;"><i class="fa fa-times-circle" aria-hidden="true"></i></a>
+	</li>
+</ul>
+<span class="dropdown-container">
 
-						<span class="dropdown-opener add-device">ADD DEVICE <i class="fa fa-caret-down" aria-hidden="true"></i></span>
+	<span class="dropdown-opener add-device"><i class="fa fa-plus" aria-hidden="true"></i> ADD ANOTHER SCREEN</span>
 
-						<nav class="dropdown xl-left">
-							<ul class="device-adder">
-								<?php
-								$db->orderBy('device_cat_order', 'asc');
-								$device_cats = $db->get('device_categories');
-								foreach ($device_cats as $device_cat) {
-									if ($device_cat['device_cat_name'] == "Custom") continue;
-								?>
+	<nav class="dropdown xl-left">
+		<ul class="device-adder">
+			<?php
+			$db->orderBy('device_cat_order', 'asc');
+			$device_cats = $db->get('device_categories');
+			foreach ($device_cats as $device_cat) {
+				if ($device_cat['device_cat_name'] == "Custom") continue;
+			?>
 
-								<li>
+			<li>
 
-									<div class="dropdown-container">
-										<div class="dropdown-opener">
-											<i class="fa <?=$device_cat['device_cat_icon']?>" aria-hidden="true"></i> <?=$device_cat['device_cat_name']?> <i class="fa fa-caret-right" aria-hidden="true"></i>
-										</div>
-										<nav class="dropdown selectable addable xl-left">
-											<ul class="device-add">
-												<?php
-												$db->where('device_cat_ID', $device_cat['device_cat_ID']);
-												$db->orderBy('device_order', 'asc');
-												$devices = $db->get('devices');
-												foreach ($devices as $device) {
-												?>
-												<li>
-													<a href="#"
-														data-device-id="<?=$device['device_ID']?>"
-														data-device-width="<?=$device['device_width']?>"
-														data-device-height="<?=$device['device_height']?>"
-														data-device-cat-name="<?=$device_cat['device_cat_name']?>"
-														data-device-cat-icon="<?=$device_cat['device_cat_icon']?>"
-													>
-														<?=$device['device_name']?> (<?=$device['device_width']?>x<?=$device['device_height']?>)
-													</a>
-												</li>
-												<?php
-												}
+				<div class="dropdown-container">
+					<div class="dropdown-opener">
+						<i class="fa <?=$device_cat['device_cat_icon']?>" aria-hidden="true"></i> <?=$device_cat['device_cat_name']?> <i class="fa fa-caret-right" aria-hidden="true"></i>
+					</div>
+					<nav class="dropdown selectable addable xl-left">
+						<ul class="device-add">
+							<?php
+							$db->where('device_cat_ID', $device_cat['device_cat_ID']);
+							$db->orderBy('device_order', 'asc');
+							$devices = $db->get('devices');
+							foreach ($devices as $device) {
+							?>
+							<li>
+								<a href="#"
+									data-device-id="<?=$device['device_ID']?>"
+									data-device-width="<?=$device['device_width']?>"
+									data-device-height="<?=$device['device_height']?>"
+									data-device-cat-name="<?=$device_cat['device_cat_name']?>"
+									data-device-cat-icon="<?=$device_cat['device_cat_icon']?>"
+								>
+									<?=$device['device_name']?> (<?=$device['device_width']?>x<?=$device['device_height']?>)
+								</a>
+							</li>
+							<?php
+							}
 
-												// Custom Device
-												if ($device_cat['device_cat_name'] == "Custom...") {
-												?>
-												<li><a href="#" data-device-id="<?=$device['device_ID']?>">Add New</a></li>
-												<?php
-												}
-												?>
-											</ul>
-										</nav>
+							// Custom Device
+							if ($device_cat['device_cat_name'] == "Custom...") {
+							?>
+							<li><a href="#" data-device-id="<?=$device['device_ID']?>">Add New</a></li>
+							<?php
+							}
+							?>
+						</ul>
+					</nav>
 
-									</div>
+				</div>
 
-								</li>
+			</li>
 
-								<?php
-								}
-								?>
-							</ul>
-						</nav>
+			<?php
+			}
+			?>
+		</ul>
+	</nav>
 
-					</span>
+</span>
+
+<?php
+}
+?>
+
+
+
+<?php
+function page_members() {
+?>
+
+
+
+<h3>Page Members <i class="fa fa-question-circle tooltip" data-tooltip="Sharing to emails that are not users is not working right now." aria-hidden="true"></i></h3>
+<div class="people">
+
+
+	<!-- Owner -->
+	<a href="<?=site_url('profile/'.User::ID()->userName)?>">
+		<picture class="profile-picture" <?=User::ID()->printPicture()?>>
+			<span <?=User::ID()->userPic != "" ? "class='has-pic'" : ""?>><?=substr(User::ID()->firstName, 0, 1).substr(User::ID()->lastName, 0, 1)?></span>
+		</picture>
+	</a>
+
+	<ul class="shares page user">
+
+	</ul>
+
+
+	<!-- Add New -->
+	<a href="#" class="new-member"><span style="font-family: Arial; font-weight: bold;">+</span></a>
+
+	<input class="share-email" type="email" data-type="page" placeholder='Type an e-mail address and hit "Enter"...' style="display: none; max-width: 75%;"/>
+
+
+	<ul class="shares page email">
+
+	</ul>
+
+
+</div>
+
 
 <?php
 }
@@ -165,34 +207,8 @@ function print_device_selector() {
 					<?=print_device_selector()?>
 
 
-					<h3>Page Members <i class="fa fa-question-circle tooltip" data-tooltip="Sharing to emails that are not users is not working right now." aria-hidden="true"></i></h3>
-					<div class="people">
+					<?=page_members()?>
 
-
-						<!-- Owner -->
-						<a href="<?=site_url('profile/'.User::ID()->userName)?>">
-							<picture class="profile-picture" <?=User::ID()->printPicture()?>>
-								<span <?=User::ID()->userPic != "" ? "class='has-pic'" : ""?>><?=substr(User::ID()->firstName, 0, 1).substr(User::ID()->lastName, 0, 1)?></span>
-							</picture>
-						</a>
-
-						<ul class="shares page user">
-
-						</ul>
-
-
-						<!-- Add New -->
-						<a href="#" class="new-member"><span style="font-family: Arial; font-weight: bold;">+</span></a>
-
-						<input class="share-email" type="email" data-type="page" placeholder='Type an e-mail address and hit "Enter"...' style="display: none; max-width: 75%;"/>
-
-
-						<ul class="shares page email">
-
-						</ul>
-
-
-					</div>
 					<br/>
 
 
@@ -237,53 +253,27 @@ function print_device_selector() {
 
 				<h3><i class="fa fa-link"></i> URL <i class="fa fa-question-circle tooltip" data-tooltip="Enter the URL you want to revise" aria-hidden="true"></i></h3>
 				<input type="url" name="page-url" placeholder="https://example.com/..." tabindex="1" autofocus required/>
-				<small class="design-uploader">or <a href="#">Upload</a> your page design <i class="fa fa-question-circle tooltip" data-tooltip="Not working yet." aria-hidden="true"></i></small>
+				<small class="design-uploader">or <a href="#" data-tooltip="Not working yet.">Upload</a> your page design <i class="fa fa-question-circle tooltip" data-tooltip="Not working yet." aria-hidden="true"></i></small>
 
 
 				<h3>Page Name <i class="fa fa-question-circle tooltip" data-tooltip="Test message" aria-hidden="true"></i></h3>
 				<input type="text" name="page-name" placeholder="e.g. Home, About, ..." tabindex="2" required/>
 
 
-				<a href="#" class="option-toggler more-options">More Options <i class="fa fa-caret-down" aria-hidden="true"></i></a>
 
-				<a href="#" class="option-toggler less-options">Less Options <i class="fa fa-caret-up" aria-hidden="true"></i></a>
+				<?=print_device_selector()?>
+
+
+				<a href="#" class="option-toggler more-options xl-right">More Options <i class="fa fa-caret-down" aria-hidden="true"></i></a>
+
+				<a href="#" class="option-toggler less-options xl-right">Less Options <i class="fa fa-caret-up" aria-hidden="true"></i></a>
 
 
 				<!-- More Options -->
 				<div class="more-options-wrapper">
 
 
-					<?=print_device_selector()?>
-
-
-					<h3>Members <i class="fa fa-question-circle tooltip" data-tooltip="Sharing to emails that are not users is not working right now." aria-hidden="true"></i></h3>
-					<div class="people">
-
-
-						<!-- Owner -->
-						<a href="<?=site_url('profile/'.User::ID()->userName)?>">
-							<picture class="profile-picture" <?=User::ID()->printPicture()?>>
-								<span <?=User::ID()->userPic != "" ? "class='has-pic'" : ""?>><?=substr(User::ID()->firstName, 0, 1).substr(User::ID()->lastName, 0, 1)?></span>
-							</picture>
-						</a>
-
-						<ul class="shares page user">
-
-						</ul>
-
-
-						<!-- Add New -->
-						<a href="#" class="new-member"><span style="font-family: Arial; font-weight: bold;">+</span></a>
-
-						<input class="share-email" type="email" data-type="page" placeholder='Type an e-mail address and hit "Enter"...' style="display: none; max-width: 75%;"/>
-
-
-						<ul class="shares page email">
-
-						</ul>
-
-
-					</div>
+					<?=page_members()?>
 					<br/>
 
 
@@ -440,7 +430,7 @@ function print_device_selector() {
 
 
 				<!-- Add New -->
-				<input data-asdasd="<?=$dataType?>" id="share-email" class="share-email" type="email" data-type="<?=$dataType?>" data-id="" placeholder='Type an e-mail address and hit "Enter"...' style="max-width: 75%;"/><br/><br/>
+				<input id="share-email" class="share-email" type="email" data-type="<?=$dataType?>" data-id="" placeholder='Type an e-mail address and hit "Enter"...' style="max-width: 75%;"/><br/><br/>
 
 
 
