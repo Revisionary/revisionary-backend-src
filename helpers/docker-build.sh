716da48b7d2bc2943abe67b9469fe621396124d1
Bilal TAS <bilaltas@me.com>
2018-12-15 17:32:56 +0300

    Different update of the permissions

--- helpers/docker-build.sh
+++ helpers/docker-build.sh
@@ -6,5 +6,6 @@ docker-compose up -d --build
 
 echo "Updating file permissions..."
 docker-compose exec www chown -R www-data:www-data /var/www/
-docker-compose exec www chmod -R g+rw /var/www/
+#docker-compose exec www chmod -R g+rw /var/www/
+docker-compose exec www chmod -R a=rwx /var/www/
 echo "File permissions updated"
