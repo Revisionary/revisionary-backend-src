0bfdf167916fe1a4c4f99b2ac91d63652783aaf4
Bilal TAS <bilaltas@me.com>
2018-12-17 14:14:33 +0300

    Site log cleaning added

--- helpers/clean-files.sh
+++ helpers/clean-files.sh
@@ -1,43 +1,65 @@
 #!/bin/bash
 
+(
 echo "Removing Composer files..."
-cd www
+cd ./www/
 rm -rf vendor
 echo "DONE: Composer files REMOVED"
+)
 
 
+(
 echo "Removing the MySQL files..."
-cd ../database/mysql/
+cd ./database/mysql/
 find . \! -name '.gitkeep' -delete
 echo "DONE: MySQL files REMOVED"
+)
 
 
+(
 echo "Removing the Session files..."
-cd ../../sessions/
+cd ./sessions/
 find . \! -name '.gitignore' -delete
 echo "DONE: Session files REMOVED"
+)
 
 
+(
 echo "Removing Node files..."
-cd ../chrome/
+cd ./chrome/
 rm -rf node_modules
 echo "DONE: Node files REMOVED"
+)
 
 
-echo "Removing the Log files..."
-cd ../logs/
+(
+echo "Removing the Apache Log files..."
+cd ./logs/
 find . \! -name '.gitignore' -delete
-echo "DONE: Log files REMOVED"
+echo "DONE: Apache Log files REMOVED"
+)
 
 
+(
+echo "Removing the Site Log files..."
+cd ./www/app/logs/
+find . \! -name '.gitignore' -delete
+echo "DONE: Site Log files REMOVED"
+)
+
+
+(
 echo "Removing the User files..."
-cd ../www/cache/users/
+cd ./www/cache/users/
 rm -rf user-
 find ./user-*/ \! -iname '*.png' -delete
 echo "DONE: User files REMOVED"
+)
 
 
+(
 echo "Removing the Project files..."
 cd ../projects/
 rm -rf project-*
 echo "DONE: Project files REMOVED"
+)
