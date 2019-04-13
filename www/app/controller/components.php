da2abdb815c443b35ddce4e407154cc7319dc60a
Bilal TAS <bilaltas@me.com>
2019-04-13 10:06:18 +0300

    CSS arrangements

--- www/app/controller/components.php
+++ www/app/controller/components.php
@@ -1,4 +1,22 @@
 <?php
 
+
+$additionalHeadJS = [
+	'process.js',
+	'revise-globals.js',
+	//'revise-functions.js',
+	'vendor/jquery-ui.min.js',
+	'vendor/Autolinker.min.js',
+	'vendor/autosize.min.js',
+	'vendor/diff.js',
+	'vendor/spectrum.js'
+];
+
+$additionalBodyJS = [
+	'vendor/jquery.mCustomScrollbar.concat.min.js',
+	'common.js',
+	'revise-page.js'
+];
+
 $page_title = "Site Components";
 require view('components');
