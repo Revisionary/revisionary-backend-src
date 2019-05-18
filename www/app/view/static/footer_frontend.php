04d04c60a98063719c83aca03ab4467400865307
Bilal TAS <bilaltas@me.com>
2019-05-18 12:49:29 +0300

    Radius added to the section borders

--- www/app/view/static/footer_frontend.php
+++ www/app/view/static/footer_frontend.php
@@ -1,18 +1,19 @@
-	<footer class="wrap xl-2 container xl-flexbox xl-middle">
-		<div class="col">
-			<div class="site-title"><a href="<?=site_url()?>" rel="home">REVISIONARY APP</a></div>
-			<div class="copyright">Copyright &copy; 2019</div>
-		</div>
-		<div class="col xl-right footer-links">
-			<nav style="margin-bottom: 10px;"><a href="/support">Support</a> | <a href="/documentation">Documentation</a> | <a href="/contact">Contact</a></nav>
-			<div class="social">
+	<footer>
+		<div class="wrap xl-2 container xl-flexbox xl-middle">
+			<div class="col">
+				<div class="site-title"><a href="<?=site_url()?>" rel="home">REVISIONARY APP</a></div>
+				<div class="copyright">Copyright &copy; 2019</div>
+			</div>
+			<div class="col xl-right footer-links">
+				<nav style="margin-bottom: 10px;"><a href="/support">Support</a> | <a href="/documentation">Documentation</a> | <a href="/contact">Contact</a></nav>
+				<div class="social">
 
-				<a href="https://twitter.com/RevisionaryApp" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-				<a href="https://www.facebook.com/RevisionaryApp/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-				<a href="https://www.instagram.com/AppRevisionary/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-				<a href="#" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
-				<a href="https://www.youtube.com/channel/UCb_wJjNMOthbCeawrSkIA9w" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
+					<a href="https://twitter.com/RevisionaryApp" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+					<a href="https://www.facebook.com/RevisionaryApp/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+					<a href="https://www.instagram.com/AppRevisionary/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href="https://www.youtube.com/channel/UCb_wJjNMOthbCeawrSkIA9w" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
 
+				</div>
 			</div>
 		</div>
 	</footer><!-- .site-footer -->
