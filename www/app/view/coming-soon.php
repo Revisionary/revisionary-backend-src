8dd28002fff920e1f30f438ecdb33001d6bacb9b
Bilal TAS <bilaltas@me.com>
2019-05-26 16:07:46 +0300

    Bug fix

--- www/app/view/coming-soon.php
+++ www/app/view/coming-soon.php
@@ -65,46 +65,48 @@ if (
 
 
 require view('static/header_html');
-
 ?>
-<main>
-	<div class="vertical-middle" style="text-align: center;">
-		<img src="<?=asset_url('images/revisionary-logo.png')?>" width="402" height="136" alt="Revisionary Logo"/>
-		<h1>We are coming soon</h1>
-		<p>Would you like to be notified when we're ready?</p>
+
+<div id="page">
+	<main>
+		<div class="vertical-middle" style="text-align: center;">
+			<img src="<?=asset_url('images/revisionary-logo.png')?>" width="402" height="136" alt="Revisionary Logo"/>
+			<h1>We are coming soon</h1>
+			<p>Would you like to be notified when we're ready?</p>
 
 
-	<?php
-	if ( isset($_GET['alreadysubscribed']) ) {
-	?>
-		<div class="xl-center" style="color: green; font-size: 30px;">You are already subscribed, we'll let you know. Thank you!</div>
-	<?php
-	} elseif ( isset($_GET['subscribed']) ) {
-	?>
-		<div class="xl-center" style="color: green; font-size: 30px;">You are now subscribed, we'll let you know when we're ready. <br/>Thank you!</div>
-	<?php
-	} else {
-	?>
-		<form action="" method="post">
+		<?php
+		if ( isset($_GET['alreadysubscribed']) ) {
+		?>
+			<div class="xl-center" style="color: green; font-size: 30px;">You are already subscribed, we'll let you know. Thank you!</div>
+		<?php
+		} elseif ( isset($_GET['subscribed']) ) {
+		?>
+			<div class="xl-center" style="color: green; font-size: 30px;">You are now subscribed, we'll let you know when we're ready. <br/>Thank you!</div>
+		<?php
+		} else {
+		?>
+			<form action="" method="post">
 
-			<div class="wrap xl-center container">
-				<div class="col xl-1-3">
-					<input type="email" name="email" class="large full" placeholder="Please enter your email address.." required/>
-				</div>
-				<div class="col">
-					<input type="submit" value="Notify Me!" class="large full" style="margin-left: -10px;"/>
-				</div>
-			</div><br/>
+				<div class="wrap xl-center container">
+					<div class="col xl-1-3">
+						<input type="email" name="email" class="large full" placeholder="Please enter your email address.." required/>
+					</div>
+					<div class="col">
+						<input type="submit" value="Notify Me!" class="large full" style="margin-left: -10px;"/>
+					</div>
+				</div><br/>
 
-			<label class="xl-hiddenn"><input type="checkbox" name="betatester" value="yes"/> <small>I would like to be a beta tester as well</small></label>
+				<label class="xl-hiddenn"><input type="checkbox" name="betatester" value="yes"/> <small>I would like to be a beta tester as well</small></label>
 
-			<input type="hidden" name="nonce" value="<?=$_SESSION["js_nonce"]?>"/>
-		</form>
-	<?php
-	}
-	?>
-	</div>
+				<input type="hidden" name="nonce" value="<?=$_SESSION["js_nonce"]?>"/>
+			</form>
+		<?php
+		}
+		?>
+		</div>
 
-</main><!-- #first-section -->
+	</main>
+</div><!-- #page -->
 
 <?php require view('static/footer_html'); ?>
