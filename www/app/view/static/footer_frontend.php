dedc037a1eac6f0585b8d37adbf824c992dedea2
Bilal TAS <bilaltas@me.com>
2019-05-26 17:39:52 +0300

    Footer navigation updated

--- www/app/view/static/footer_frontend.php
+++ www/app/view/static/footer_frontend.php
@@ -5,7 +5,7 @@
 				<div class="copyright">Copyright &copy; 2019</div>
 			</div>
 			<div class="col xl-right footer-links">
-				<nav style="margin-bottom: 10px;"><a href="/support">Support</a> | <a href="/documentation">Documentation</a> | <a href="/contact">Contact</a></nav>
+				<nav style="margin-bottom: 10px;"><a href="/help">Get Started</a> | <a href="/pricing">Pricing</a> | <a href="/contact">Contact</a></nav>
 				<div class="social">
 
 					<a href="https://twitter.com/RevisionaryApp" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
