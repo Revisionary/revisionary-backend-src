5ae5a4398bedff3553327369c69032dab2639f82
Bilal TAS <bilaltas@me.com>
2019-09-23 01:17:33 +0300

    Deployment arrangements

--- helpers/docker-build.sh
+++ helpers/docker-build.sh
@@ -4,11 +4,11 @@
 docker-compose up -d --build
 
 
-echo "Updating file permissions..."
+echo "Updating file permissions in server..."
 docker-compose exec www chown -R www-data:www-data /var/www/
 docker-compose exec www find /var/www/ -type f -exec chmod 644 {} \;
 docker-compose exec www find /var/www/ -type d -exec chmod 755 {} \;
 
 #docker-compose exec www chmod -R g+rw /var/www/
 #docker-compose exec www chmod -R a=rwx /var/www/
-echo "File permissions updated"
+echo "File permissions updated in server"
