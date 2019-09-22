d1ff27c33af555d8a84072463ded59af3117b66d
Bilal TAS <bilaltas@me.com>
2019-09-23 00:33:45 +0300

    Arrangements on limitations

--- www/app/model/limitations.php
+++ www/app/model/limitations.php
@@ -1,9 +1,14 @@
 <?php
 
+$limit_user_ID = currentUserID();
+//$limit_user_ID = 1;
+$limitUser = User::ID($limit_user_ID);
+
+
 // PROJECTS
-$maxProjects = getUserInfo()['userLevelMaxProject'];
-$myProjects = array_filter($User->getProjects(), function($projectFound) {
-	return $projectFound['user_ID'] == currentUserID();
+$maxProjects = getUserInfo($limit_user_ID)['userLevelMaxProject'];
+$myProjects = array_filter($limitUser->getProjects(), function($projectFound) use ($limit_user_ID) {
+	return $projectFound['user_ID'] == $limit_user_ID;
 });
 $projectsCount = count( $myProjects );
 $projectsPercentage = intval((100 * $projectsCount) / $maxProjects);
@@ -11,9 +16,9 @@ $maxProjects = $maxProjects == 99999 ? "∞" : $maxProjects;
 
 
 // PAGES (PHASES)
-$maxPhases = getUserInfo()['userLevelMaxPage'];
-$myPhases = array_filter($User->getPhases(), function($phaseFound) {
-	return $phaseFound['user_ID'] == currentUserID();
+$maxPhases = getUserInfo($limit_user_ID)['userLevelMaxPage'];
+$myPhases = array_filter($limitUser->getPhases(), function($phaseFound) use ($limit_user_ID) {
+	return $phaseFound['user_ID'] == $limit_user_ID;
 });
 $phasesCount = count( $myPhases );
 $phasesPercentage = intval((100 * $phasesCount) / $maxPhases);
@@ -21,21 +26,18 @@ $maxPhases = $maxPhases == 99999 ? "∞" : $maxPhases;
 
 
 // PINS
-$maxPins = getUserInfo()['userLevelMaxLivePin'];
-$myPins = array_filter($User->getPins(), function($pinFound) {
-	return $pinFound['user_ID'] == currentUserID() && $pinFound['pin_type'] == 'live';
+$maxPins = getUserInfo($limit_user_ID)['userLevelMaxLivePin'];
+$myPins = array_filter($limitUser->getPins(), function($pinFound) use ($limit_user_ID) {
+	return $pinFound['user_ID'] == $limit_user_ID && $pinFound['pin_type'] == 'live';
 });
 $pinsCount = count( $myPins );
 $pinsPercentage = intval((100 * $pinsCount) / $maxPins);
 $maxPins = $maxPins == 99999 ? "∞" : $maxPins;
 
 
-// LOAD !!! CACHE THIS
-$maxLoad = getUserInfo()['userLevelMaxLoad'];
-
-
-// CHECK THE CACHE FIRST
-$cached_userLoad = $cache->get('userload:'.currentUserID());
+// LOAD
+$maxLoad = getUserInfo($limit_user_ID)['userLevelMaxLoad'];
+$cached_userLoad = $cache->get('userload:'.$limit_user_ID);
 if ( $cached_userLoad !== false ) $loadCount = $cached_userLoad;
 else {
 
@@ -52,7 +54,7 @@ else {
 	$loadCount = $filesLoadMb;
 
 	// Set the cache
-	$cache->set('userload:'.currentUserID(), $loadCount);
+	$cache->set('userload:'.$limit_user_ID, $loadCount);
 
 }
 $loadPercentage = intval((100 * $loadCount) / $maxLoad);
