29101e5c9cd78ce897d81a5eec68dbb43e30640d
Bilal TAS <bilaltas@me.com>
2019-11-10 20:25:32 +0300

    Device tooltip to the bottom

--- www/app/view/modules/categorized_blocks.php
+++ www/app/view/modules/categorized_blocks.php
@@ -565,7 +565,7 @@
 
 												<span class="item device-wrap" data-type="device" data-id="<?=$device['device_ID']?>">
 													<a href="<?=site_url('revise/'.$device['device_ID'])?>" class="device-link">
-														<i class="fa <?=$device['screen_cat_icon']?>" data-tooltip="<?=$device['screen_cat_name']?> (<?=$screenWidth?>x<?=$screenHeight?>)"></i>
+														<i class="fa <?=$device['screen_cat_icon']?> bottom-tooltip" data-tooltip="<?=$device['screen_cat_name']?> (<?=$screenWidth?>x<?=$screenHeight?>)"></i>
 													</a>
 													<a href="<?=site_url($action_url.'&action=remove')?>" data-tooltip="Delete This Screen" class="remove-device" data-action="remove" data-confirm="Are you sure you want to completely remove this screen? Keep in mind that no one will be able to access this device and its pins anymore!"><i class="fa fa-times-circle"></i></a>
 												</span>
