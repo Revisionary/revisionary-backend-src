a488679d41c30f69f8924cf8796aefd780634a13
Bilal TAS <bilaltas@me.com>
2019-11-22 08:40:49 +0300

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// https://gist.github.com/leymannx/f7867942184d01aa2311
+
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    prefix      = require('gulp-autoprefixer'),
+    plumber     = require('gulp-plumber'),
+    sassLint    = require('gulp-sass-lint'),
+    sourcemaps  = require('gulp-sourcemaps');
+
+
+// SETTINGS
+// ---------------
+
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+
+// BUILD SUBTASKS
+// ---------------
+
+gulp.task('styles', function() {
+	return gulp.src('./www/assets/styles/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass(sassOptions))
+		.pipe(prefix())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./www/assets/styles/'));
+});
+
+gulp.task('sass-lint', function() {
+	gulp.src('./www/assets/styles/*.scss')
+		.pipe(sassLint())
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./www/assets/styles/style.scss', gulp.series('styles'));
+	gulp.watch('./www/assets/styles/revise.scss', gulp.series('styles'));
+	//gulp.watch('scss/**/*.scss', gulp.series('styles'));
+});
+
+
+// BUILD TASKS
+// ------------
+
+gulp.task('default', gulp.series('styles', 'watch'));
+gulp.task('build', gulp.series('styles'));
