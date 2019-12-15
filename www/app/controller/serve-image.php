4bac47dd90d8e118cad8034d9cfd8cded8ed96d7
Bilal TAS <bilaltas@me.com>
2019-12-15 03:27:28 +0300

    Design upload development

---
+++ www/app/controller/serve-image.php
@@ -0,0 +1,21 @@
+<?php
+
+$device_ID = intval( get('device_ID') );
+
+
+$deviceData = Device::ID($device_ID);
+if (!$deviceData) return false;
+$device = $deviceData->getInfo();
+//die_to_print($device);
+
+
+
+
+$image_path = "projects/project-".$device['project_ID']."/page-".$device['page_ID']."/phase-".$device['phase_ID']."/screenshots/device-".$device['device_ID'].".jpg";
+$image_url = cache_url($image_path);
+//die_to_print($image_url);
+
+
+
+$page_title = "Page Design";
+require view('serve-image');
---
+++ www/app/view/serve-image.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?=$page_title?></title>
+
+	<style>
+
+		html, body {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+		}
+
+		img {
+			max-width: 100%;
+		}
+
+	</style>
+
+</head>
+<body data-revisionary-index="0">
+
+	<img src="<?=$image_url?>" alt="">
+	
+</body>
+</html>
