4dfe2f56c1bb872cc18ace7c5170208538ec11f6
Bilal TAS <bilaltas@me.com>
2020-01-31 17:06:05 +0300

    FAQ added to help menus

--- www/app/view/modules/help-menu.php
+++ www/app/view/modules/help-menu.php
@@ -3,5 +3,6 @@
 <li><a href="#" data-modal="video" data-tooltip="Coming soon..."><i class="fas fa-layer-group"></i> Advanced Features</a></li>
 <li><a href="#" data-modal="video" data-tooltip="Coming soon..."><i class="fas fa-keyboard"></i> Keyboard Shortcuts</a></li>
 <li><a href="#" data-modal="video" data-tooltip="Coming soon..."><i class="fas fa-info"></i> Integrations Help</a></li>
-<li><a href="#" data-modal="feedback"><i class="fa fa-comments"></i> Feedback</a></li>
+<li><a href="#" data-modal="video" data-tooltip="Coming soon..."><i class="fas fa-question"></i> FAQ</a></li>
 <li><a href="#" data-tooltip="Coming soon..."><i class="fas fa-life-ring"></i> Contact Support</a></li>
+<li><a href="#" data-modal="feedback"><i class="fa fa-comments"></i> Feedback</a></li>
