2c1aa606436f0aacf8eadbe9c838f6937e505a63
Bilal TAS <bilaltas@me.com>
2020-03-08 18:15:03 +0300

    FontAwesome updated

--- www/app/view/static/footer_frontend.php
+++ www/app/view/static/footer_frontend.php
@@ -17,7 +17,7 @@
 					<a href="https://www.linkedin.com/company/revisionaryapp/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 					<a href="https://twitter.com/RevisionaryApp" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 					<a href="https://www.facebook.com/RevisionaryApp/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-					<a href="https://www.instagram.com/AppRevisionary/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href="https://www.instagram.com/AppRevisionary/" target="_blank"><i class="fab fa-instagram-square"></i></a>
 					<a href="https://www.youtube.com/channel/UCb_wJjNMOthbCeawrSkIA9w" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
 
 				</div>
