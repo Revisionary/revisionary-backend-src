0932d0228152abd756c92a05359a8b6447b3f238
Bilal TAS <bilaltas@me.com>
2020-03-08 20:02:22 +0300

    Initial FAQ questions added

--- www/app/view/faq.php
+++ www/app/view/faq.php
@@ -5,35 +5,19 @@
 
 		<div class="xl-center"><br>
 			<h1>Frequently Asked Questions</h1>
-			<!-- <p>Find the features you'll need to make your clients happy.</p> -->
 		</div>
 
 
 		<div class="wrap xl-2 xl-gutter-40">
 			<div class="col">
 
-				<h2 class="h5">What does Revisionary App store?</h2>
+				<h2 class="h5">What are the projects?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
 			<div class="col">
 
-				<h2 class="h5">What does Revisionary App store?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
-
-			</div>
-		</div>
-
-		<div class="wrap xl-2 xl-gutter-40">
-			<div class="col">
-
-				<h2 class="h5">What does Revisionary App store?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
-
-			</div>
-			<div class="col">
-
-				<h2 class="h5">What does Revisionary App store?</h2>
+				<h2 class="h5">What are the pages?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
@@ -42,13 +26,43 @@
 		<div class="wrap xl-2 xl-gutter-40">
 			<div class="col">
 
-				<h2 class="h5">What does Revisionary App store?</h2>
+				<h2 class="h5">What are the phases?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
 			<div class="col">
 
-				<h2 class="h5">What does Revisionary App store?</h2>
+				<h2 class="h5">What are the devices?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+		</div>
+
+		<div class="wrap xl-2 xl-gutter-40">
+			<div class="col">
+
+				<h2 class="h5">What are the pins?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+			<div class="col">
+
+				<h2 class="h5">What does Revisionary store?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+		</div>
+
+		<div class="wrap xl-2 xl-gutter-40">
+			<div class="col">
+
+				<h2 class="h5">Does it work with JS websites like React or Vue JS?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+			<div class="col">
+
+				<h2 class="h5">Should I add a new page to start again?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
