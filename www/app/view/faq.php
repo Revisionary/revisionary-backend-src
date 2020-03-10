1de9ee602541c96b1bf6f5022048b2d70720b1b6
Bilal TAS <bilaltas@me.com>
2020-03-10 10:59:18 +0300

    FAQ updates

--- www/app/view/faq.php
+++ www/app/view/faq.php
@@ -4,9 +4,24 @@
 	<div class="container">
 
 		<div class="xl-center"><br>
-			<h1>Frequently Asked Questions</h1>
+			<h1>Frequently Asked Questions</h1><br>
 		</div>
 
+
+		<div class="wrap xl-2 xl-gutter-40">
+			<div class="col">
+
+				<h2 class="h5">What is the load limit?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+			<div class="col">
+
+				<h2 class="h5">What does Revisionary store?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+
+			</div>
+		</div>
 
 		<div class="wrap xl-2 xl-gutter-40">
 			<div class="col">
@@ -32,7 +47,7 @@
 			</div>
 			<div class="col">
 
-				<h2 class="h5">What are the devices?</h2>
+				<h2 class="h5">What are the devices & screens?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
@@ -47,7 +62,7 @@
 			</div>
 			<div class="col">
 
-				<h2 class="h5">What does Revisionary store?</h2>
+				<h2 class="h5">Does it work with JS websites like React or Vue JS?</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
 
 			</div>
@@ -56,14 +71,14 @@
 		<div class="wrap xl-2 xl-gutter-40">
 			<div class="col">
 
-				<h2 class="h5">Does it work with JS websites like React or Vue JS?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+				<h2 class="h5">What can I do with the Free plan?</h2>
+				<p>You can do almost everthing with the Free plan. But, there are some limitations on amount of Projects, Pages, Screens, Pins, and the load size of your webpage.</p>
 
 			</div>
 			<div class="col">
 
-				<h2 class="h5">Should I add a new page to start again?</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis amet eos vero qui unde, dignissimos, ea soluta odit maiores rem ab fugiat! Quae temporibus cumque debitis incidunt, nesciunt sequi provident?</p>
+				<h2 class="h5">Any other questions?</h2>
+				<p>For more information, please feel free to contact us at <a href="mailto:info@revisionaryapp.com" class="text-color-secondary">info@revisionaryapp.com</a></p>
 
 			</div>
 		</div>
