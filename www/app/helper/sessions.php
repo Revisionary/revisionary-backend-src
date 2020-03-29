ecf8dbfad0d864ac275bbd186af2252d0432cf30
Bilal TAS <bilaltas@me.com>
2020-03-30 00:28:08 +0300

    Load session updates

--- www/app/helper/sessions.php
+++ www/app/helper/sessions.php
@@ -1,29 +1,28 @@
 <?php
 
+function setup_session() {
+
+    session_name(session_name);
+    //session_save_path(sessiondir);
+    ini_set('session.cookie_domain', '.'.domain);
+    ini_set('session.gc_maxlifetime', session_lifetime);
+    session_set_cookie_params(0, '/', '.'.domain);
+    session_start();
+    
+}
+
 function load_session() {
 
 
     if (session_status() == PHP_SESSION_NONE) {
 
-        session_name(session_name);
-        session_save_path(sessiondir);
-		ini_set('session.cookie_domain', '.'.domain);
-		ini_set('session.gc_maxlifetime', session_lifetime);
-        session_set_cookie_params(0, '/', '.'.domain);
-        session_start();
+        setup_session();
 
-    } else {
-        if (session_name() != session_name) {
+    } else if (session_name() != session_name) {
 
-            session_destroy();
-            session_name(session_name);
-            session_save_path(sessiondir);
-			ini_set('session.cookie_domain', '.'.domain);
-			ini_set('session.gc_maxlifetime', session_lifetime);
-            session_set_cookie_params(0, '/', '.'.domain);
-            session_start();
+        session_destroy();
+        setup_session();
 
-        }
     }
 
 
