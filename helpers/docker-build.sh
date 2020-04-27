d4e4c4970d32eee36cdca1c3d09f7d8eb25cf3f9
Bilal TAS <bilaltas@me.com>
2020-04-27 17:35:33 +0300

    Upgrade the DB class

--- helpers/docker-build.sh
+++ helpers/docker-build.sh
@@ -5,10 +5,7 @@ docker-compose up -d --build
 
 
 echo "Updating file permissions in server..."
-docker-compose exec www chown -R www-data:www-data /var/www/
-docker-compose exec www find /var/www/ -type f -exec chmod 644 {} \;
-docker-compose exec www find /var/www/ -type d -exec chmod 755 {} \;
-
-#docker-compose exec www chmod -R g+rw /var/www/
-#docker-compose exec www chmod -R a=rwx /var/www/
+docker-compose exec backend chown -R www-data:www-data /var/www/
+docker-compose exec backend find /var/www/ -type f -exec chmod 644 {} \;
+docker-compose exec backend find /var/www/ -type d -exec chmod 755 {} \;
 echo "File permissions updated in server"
