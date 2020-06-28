d4f508f76067816dd040d7cd4d1c0f04d758c6af
Bilal TAS <bilaltas@me.com>
2020-06-28 19:54:18 +0300

    Bug fix

--- app/controller/test-notification-update.php
+++ app/controller/test-notification-update.php
@@ -7,18 +7,18 @@ $notifications = $db->get('notifications');
 
 foreach($notifications as $n) {
 
-    $db->where ('notification_ID', $n['notification_ID']);
+    $db->where('notification_ID', $n['notification_ID']);
 
     print_r($n);
     if ($n['object_type'] == "project")
-        $db->update('notifications', array("project_ID" => $n['object_ID']));
+        $db->update('notifications', array("project_ID" => intval($n['object_ID'])));
 
     //print_r($n);
     elseif ($n['object_type'] == "page")
-        $db->update('notifications', array("page_ID" => $n['object_ID']));
+        $db->update('notifications', array("page_ID" => intval($n['object_ID'])));
 
     //print_r($n);
     elseif ($n['object_type'] == "pin")
-        $db->update('notifications', array("pin_ID" => $n['object_ID']));
+        $db->update('notifications', array("pin_ID" => intval($n['object_ID'])));
 
 }
