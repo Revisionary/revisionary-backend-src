14a1d8b68e4f05b982e7214f178eb91e8733d322
Bilal Tas <bilaltas@me.com>
2020-08-02 16:36:28 +0300

    Allow from everywhere

--- app/controller/v1.php
+++ app/controller/v1.php
@@ -4,7 +4,7 @@
 
 // Headers
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: https://".$config['env']['dashboard_subdomain'].".".$config['env']['dashboard_domain']);
+//header("Access-Control-Allow-Origin: https://".$config['env']['dashboard_subdomain'].".".$config['env']['dashboard_domain']);
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 header("Access-Control-Max-Age: 3600");
