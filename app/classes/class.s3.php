c93463e51971c3b6a39602fd4e4b4b31a50a3bc7
Bilal TAS <bilaltas@me.com>
2021-08-15 14:43:32 +0300

    Bug fixes

--- app/classes/class.s3.php
+++ app/classes/class.s3.php
@@ -1,11 +1,11 @@
 <?php
 
-class Space {
+// class Space {
 
 
 
-}
+// }
 
 // Configure a client using Spaces
-if ($config['env']['s3_key']) 
+if ($config['env']['s3_key'])
 	$s3 = new SpacesConnect($config['env']['s3_key'], $config['env']['s3_secret'], $config['env']['s3_bucket'], $config['env']['s3_region']);
